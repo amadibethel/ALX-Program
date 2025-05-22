cb925dd211cc3eee2fa6ea577c89a1176d12e0b1
Bethel Amadi <amadi_bethel@icloud.com>
2025-05-22 10:43:27 +0200

    Add script to demonstrate JavaScript practice

---
+++ alx_practice.js
@@ -0,0 +1,20 @@
+const name = "Bethel Amadi";
+const age = 30;
+const isStudent = true;
+
+const address = {
+  city: "Cape Town",
+  country: "South Africa"
+};
+
+const hobbies = ["coding", "reading", "traveling"];
+let undefinedVariable;
+const nullVariable = null;
+
+console.log("name:", name, "| type:", typeof name);
+console.log("age:", age, "| type:", typeof age);
+console.log("isStudent:", isStudent, "| type:", typeof isStudent);
+console.log("address:", address, "| type:", typeof address);
+console.log("hobbies:", hobbies, "| type:", typeof hobbies);
+console.log("undefinedVariable:", undefinedVariable, "| type:", typeof undefinedVariable);
+console.log("nullVariable:", nullVariable, "| type:", typeof nullVariable);
